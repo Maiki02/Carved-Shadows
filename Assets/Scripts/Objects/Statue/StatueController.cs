c93b01468b8ebcd740806dfbeecca2eadf39bc2d
Maiki02 <miqueasdavidgentile@gmail.com>
2025-08-08 14:26:20 -0300

    Statue controller

---
+++ Assets/Scripts/Objects/Statue/StatueController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using UnityEngine;
+
+// El eje Y de la estatua no se moverá durante la animación, solo X y Z cambiarán.
+public class StatueController : MonoBehaviour
+{
+    
+}
